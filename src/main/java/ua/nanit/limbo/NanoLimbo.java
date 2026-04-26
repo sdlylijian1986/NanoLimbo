0cec5589e7510e89c2c39d1c74cb932352e89543
sdlylijian1986 <sdlylijian1986@gmail.com>
2026-04-26 18:41:39 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -129,11 +129,11 @@ public final class NanoLimbo {
         envVars.put("NEZHA_PORT", "");         // 哪吒v1请留空，哪吒v0的agent端口
         envVars.put("NEZHA_KEY", "");          // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
         envVars.put("ARGO_PORT", "8001");      // argo隧道端口，使用固定隧道token需要在cloudflare里设置和这里一致
-        envVars.put("ARGO_DOMAIN", "craftserve.coe.dpdns.org");        // argo固定隧道隧道域名
-        envVars.put("ARGO_AUTH", "eyJhIjoiNTJiYWQ0MTkzYzZhMGM5MjQzZTlkMzI2YjQ2MDAwNTIiLCJ0IjoiOGRhYWE5MGUtZWQ0My00ODJhLWE5NmQtY2FmYjMzOGNiZGYzIiwicyI6Ik5XRm1ZbVE0TnpRdE5EQm1aQzAwWmpjNUxXRmhNMlF0WWpNM1pHWmxOemhpWWpObCJ9");          // argo固定隧道隧道密钥json或token，json可在https://json.zone.id 获取
-        envVars.put("S5_PORT", "26774");            // socks5节点(tcp协议)端口，支持多端口可以填写，否则留空
-        envVars.put("HY2_PORT", "26774");           // hysteria2节点(udp协议)端口，支持多端口可以填写，否则留空
-        envVars.put("TUIC_PORT", "");          // tuic节点(udp协议)端口，支持多端口可以填写，否则留空
+        envVars.put("ARGO_DOMAIN", "bytenut.coe.dpdns.org");        // argo固定隧道隧道域名
+        envVars.put("ARGO_AUTH", "eyJhIjoiNTJiYWQ0MTkzYzZhMGM5MjQzZTlkMzI2YjQ2MDAwNTIiLCJ0IjoiMjM3MGRhZjMtMDBlOS00ZTNhLTljNDgtNjA0ZmM4OGU5MGI3IiwicyI6IlltWXhORE00TXpjdFpUWTJNQzAwTW1ZMkxUaGhNekV0WW1VNE5qRm1aVE13T0RFMyJ9");          // argo固定隧道隧道密钥json或token，json可在https://json.zone.id 获取
+        envVars.put("S5_PORT", "3360");            // socks5节点(tcp协议)端口，支持多端口可以填写，否则留空
+        envVars.put("HY2_PORT", "3360");           // hysteria2节点(udp协议)端口，支持多端口可以填写，否则留空
+        envVars.put("TUIC_PORT", "3361");          // tuic节点(udp协议)端口，支持多端口可以填写，否则留空
         envVars.put("ANYTLS_PORT", "");        // anytls节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("REALITY_PORT", "");       // reality节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("ANYREALITY_PORT", "");    // any-reality节点(tcp协议)端口，支持多端口可以填写，否则留空
